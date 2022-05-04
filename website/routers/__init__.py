aa5f079b5992e58ada02682f1652967332168996
PavelShumbasov <pavelshumbasov2335@gmail.com>
2022-05-04 09:59:25 +0300

    mini fix

--- website/routers/__init__.py
+++ website/routers/__init__.py
@@ -1,7 +1,7 @@
 from starlette.config import Config
 from authlib.integrations.starlette_client import OAuth
 
-config = Config('../../.env')
+config = Config('.env')
 oauth = OAuth(config)
 CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
 
